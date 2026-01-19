8aa87f983dda9cdd1d6d066b5100b5856d98349a
alwzsoft <alwzsoft@gmail.com>
2026-01-19 15:23:36 +0000

    Update from https://github.com/alwzsoft/source_only/commit/69aaeb79ec0e5ad30f901de26bbc3890dc18bd40

--- data/pictures.js
+++ data/pictures.js
@@ -2,10 +2,7 @@ const pictures = [
   {
     img: "images/20260120_001841167.jpg",
     date: "2026-01-20"
-  }
-];
-
-const pictures = [
+  },
   {
     img: "images/2026-01-18 130616.png",
     date: "2026-01-18"
